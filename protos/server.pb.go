1ed3b59c6342ca004701a7f8a4afc35c0dc6181d
零号 <jinjianfeng@cai-inc.com>
2023-05-17 16:29:29 +0800

    modify response

--- protos/server.pb.go
+++ protos/server.pb.go
@@ -31,13 +31,13 @@ var file_protos_server_proto_rawDesc = []byte{
 	0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2f, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x2e, 0x70, 0x72,
 	0x6f, 0x74, 0x6f, 0x1a, 0x15, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2f, 0x72, 0x65, 0x73, 0x70,
 	0x6f, 0x6e, 0x73, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x32, 0x6c, 0x0a, 0x0a, 0x47, 0x72,
-	0x70, 0x63, 0x53, 0x65, 0x72, 0x76, 0x65, 0x72, 0x12, 0x33, 0x0a, 0x0a, 0x53, 0x65, 0x6e, 0x64,
-	0x53, 0x74, 0x72, 0x65, 0x61, 0x6d, 0x12, 0x0f, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e,
-	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x10, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73,
-	0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x28, 0x01, 0x30, 0x01, 0x12, 0x29, 0x0a,
-	0x04, 0x53, 0x65, 0x6e, 0x64, 0x12, 0x0f, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e, 0x52,
-	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x10, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e,
-	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x42, 0x09, 0x5a, 0x07, 0x2f, 0x70, 0x72, 0x6f,
+	0x70, 0x63, 0x53, 0x65, 0x72, 0x76, 0x65, 0x72, 0x12, 0x29, 0x0a, 0x04, 0x53, 0x65, 0x6e, 0x64,
+	0x12, 0x0f, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x1a, 0x10, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f,
+	0x6e, 0x73, 0x65, 0x12, 0x33, 0x0a, 0x0a, 0x53, 0x65, 0x6e, 0x64, 0x53, 0x74, 0x72, 0x65, 0x61,
+	0x6d, 0x12, 0x0f, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65,
+	0x73, 0x74, 0x1a, 0x10, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2e, 0x52, 0x65, 0x73, 0x70,
+	0x6f, 0x6e, 0x73, 0x65, 0x28, 0x01, 0x30, 0x01, 0x42, 0x09, 0x5a, 0x07, 0x2f, 0x70, 0x72, 0x6f,
 	0x74, 0x6f, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
@@ -46,10 +46,10 @@ var file_protos_server_proto_goTypes = []interface{}{
 	(*Response)(nil), // 1: protos.Response
 }
 var file_protos_server_proto_depIdxs = []int32{
-	0, // 0: protos.GrpcServer.SendStream:input_type -> protos.Request
-	0, // 1: protos.GrpcServer.Send:input_type -> protos.Request
-	1, // 2: protos.GrpcServer.SendStream:output_type -> protos.Response
-	1, // 3: protos.GrpcServer.Send:output_type -> protos.Response
+	0, // 0: protos.GrpcServer.Send:input_type -> protos.Request
+	0, // 1: protos.GrpcServer.SendStream:input_type -> protos.Request
+	1, // 2: protos.GrpcServer.Send:output_type -> protos.Response
+	1, // 3: protos.GrpcServer.SendStream:output_type -> protos.Response
 	2, // [2:4] is the sub-list for method output_type
 	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -95,8 +95,8 @@ const _ = grpc.SupportPackageIsVersion6
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type GrpcServerClient interface {
-	SendStream(ctx context.Context, opts ...grpc.CallOption) (GrpcServer_SendStreamClient, error)
 	Send(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+	SendStream(ctx context.Context, opts ...grpc.CallOption) (GrpcServer_SendStreamClient, error)
 }
 
 type grpcServerClient struct {
@@ -105,6 +105,15 @@ type grpcServerClient struct {
 
 func NewGrpcServerClient(cc grpc.ClientConnInterface) GrpcServerClient {
 	return &grpcServerClient{cc}
+}
+
+func (c *grpcServerClient) Send(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/protos.GrpcServer/Send", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 func (c *grpcServerClient) SendStream(ctx context.Context, opts ...grpc.CallOption) (GrpcServer_SendStreamClient, error) {
@@ -138,34 +147,43 @@ func (x *grpcServerSendStreamClient) Recv() (*Response, error) {
 	return m, nil
 }
 
-func (c *grpcServerClient) Send(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
-	out := new(Response)
-	err := c.cc.Invoke(ctx, "/protos.GrpcServer/Send", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // GrpcServerServer is the server API for GrpcServer service.
 type GrpcServerServer interface {
-	SendStream(GrpcServer_SendStreamServer) error
 	Send(context.Context, *Request) (*Response, error)
+	SendStream(GrpcServer_SendStreamServer) error
 }
 
 // UnimplementedGrpcServerServer can be embedded to have forward compatible implementations.
 type UnimplementedGrpcServerServer struct {
 }
 
-func (*UnimplementedGrpcServerServer) SendStream(GrpcServer_SendStreamServer) error {
-	return status.Errorf(codes.Unimplemented, "method SendStream not implemented")
-}
 func (*UnimplementedGrpcServerServer) Send(context.Context, *Request) (*Response, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Send not implemented")
+}
+func (*UnimplementedGrpcServerServer) SendStream(GrpcServer_SendStreamServer) error {
+	return status.Errorf(codes.Unimplemented, "method SendStream not implemented")
 }
 
 func RegisterGrpcServerServer(s *grpc.Server, srv GrpcServerServer) {
 	s.RegisterService(&_GrpcServer_serviceDesc, srv)
+}
+
+func _GrpcServer_Send_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Request)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GrpcServerServer).Send(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/protos.GrpcServer/Send",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GrpcServerServer).Send(ctx, req.(*Request))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _GrpcServer_SendStream_Handler(srv interface{}, stream grpc.ServerStream) error {
@@ -192,24 +210,6 @@ func (x *grpcServerSendStreamServer) Recv() (*Request, error) {
 		return nil, err
 	}
 	return m, nil
-}
-
-func _GrpcServer_Send_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Request)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(GrpcServerServer).Send(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/protos.GrpcServer/Send",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GrpcServerServer).Send(ctx, req.(*Request))
-	}
-	return interceptor(ctx, in, info, handler)
 }
 
 var _GrpcServer_serviceDesc = grpc.ServiceDesc{
